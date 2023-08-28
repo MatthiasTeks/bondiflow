bec2a3fb00550f916dae127a807be6834c8db374
Matthias <matthias.vimbert@gmail.com>
2023-08-28 14:16:23 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ const htmlmin = require('gulp-htmlmin');
 const cssnano = require('gulp-cssnano');
 
 gulp.task('minify-js', function () {
-    return gulp.src('src/*.js')
+    return gulp.src('js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('minify-html', function () {
-    return gulp.src('src/*.html')
+    return gulp.src(['*.html', '404.html', '401.html'])
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', function () {
-    return gulp.src('src/*.css')
+    return gulp.src('css/*.css')
         .pipe(cssnano())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('default', gulp.parallel('minify-js', 'minify-html', 'minify-css'));
